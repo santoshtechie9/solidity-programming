0ce168f20843e4276e265633ae240b6068ff3925
santoshtechie9 <santoshtechie9@gmail.com>
2017-10-05 00:01:23 -0700

    created contracts

---
+++ contracts/instructor.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.4.17
+
+contract Instructor {
+
+    string public instructorName;
+
+    function Instructor(string _name) {
+        instructorName = _name;
+    }
+
+}
---
+++ contracts/participant.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.4.17
+
+contract Participant {
+
+    string public participantName;
+
+    function Participant(string _name){
+        participantName = _name;
+    }
+
+}
