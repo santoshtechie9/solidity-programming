23d957c4cd988cd5969d7afc6770d3598e8b1a55
santoshtechie9 <santoshtechie9@gmail.com>
2017-10-06 08:57:37 -0700

    solidity examples

---
+++ helloworld.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.15;
+
+contract HelloWorld {
+
+    string public greeting;
+
+    function HelloWold(string _text) {
+        greeting = _text;
+    }
+
+    function getGreeting() constant returns (string) {
+        return greeting;
+    }
+
+}
