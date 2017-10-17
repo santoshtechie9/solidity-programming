ca3cf2a210d0e3709df44ace05f8f755dcb24248
Santosh <santoshtechie9@gmail.com>
2017-10-17 08:56:12 -0700

    Update 1-demo.sol

--- examples/1-demo.sol
+++ examples/1-demo.sol
@@ -5,10 +5,11 @@ Contract is the highest level object in Ethereum and everything else will be enc
 
 Steps to deploy a contract:
 ---------------------------
-    1. Open mist wallet.
-	2. Go to contracts tab.
-	3. Copy the above contract and paste it in contracts.
-    4. Deploy it in test net.
+1. Open mist wallet.
+2. Go to contracts tab.
+3. Copy the above contract and paste it in contracts.
+4. Deploy it in test net.
+
 */
 
 contract User {
