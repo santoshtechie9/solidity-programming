2602d98be6ec85c5b433505ad73354f9dab95e9b
Santosh <santoshtechie9@gmail.com>
2017-10-17 08:55:12 -0700

    Update 1-demo.sol

--- examples/1-demo.sol
+++ examples/1-demo.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.4.14;
 
+/*
+Contract is the highest level object in Ethereum and everything else will be enclosed in a contract(Variables, objects, transactions etc.)
+
+Steps to deploy a contract:
+---------------------------
+    1. Open mist wallet.
+	2. Go to contracts tab.
+	3. Copy the above contract and paste it in contracts.
+    4. Deploy it in test net.
+*/
+
 contract User {
 
     string public userName;
