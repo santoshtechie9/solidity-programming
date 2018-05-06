f591bfb7e446b1f7f1a5087b56adda612e3734b8
santoshtechie9@gmail.com <September*09>
2018-05-05 22:20:57 -0700

    init commit

---
+++ solidity_basics/1-FirstContract.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.22;
+
+contract FirstContract {
+
+    //instance variables
+    address ownerAddress;
+    uint data;
+
+    //event definition
+    event logData(uint256 dataToLog);
+
+    //function modifier
+    modifier onlyOwner() {
+        
+        //require throws an exception when the condition evaluates to false
+        require(
+            msg.sender == ownerAddress,
+            "only owner can modify address"
+            );
+        _;
+    }
+    
+    //constructor
+    constructor(uint256 initData, address initOwner) public {
+        data = initData;
+        ownerAddress = initOwner;
+    }
+
+    function getData() public returns (uint returnedData) {
+        return data;
+    }
+
+    function setData(uint newData) onlyOwner public {
+        data = newData;
+    }
+
+}
+
